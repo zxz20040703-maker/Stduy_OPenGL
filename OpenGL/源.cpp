052677489a01f6693505338077810a82157b5883
uman445 <zxz20040703@163.com>
2026-05-25 21:18:12 +0800

    理解了索引缓冲区IBO和顶点缓冲区。以及GLSL的基础知识VBO
    通过gl提供的一系列api，可以把对应文本直接编译成GLSL，十分方便。

--- OpenGL/源.cpp
+++ OpenGL/源.cpp
@@ -1,28 +1,119 @@
 #include<GL/glew.h>
 #include <GLFW/glfw3.h>
 #include<iostream>
+#include<fstream>
+#include<string>
+#include<sstream>
 
-int main(void)
+
+struct ShaderProgramResource
+{
+	std::string VertexShader;
+	std::string FragmentShader;
+};
+
+static ShaderProgramResource ParseShader(std::string filePath)
+{
+	enum class ShaderType
+	{
+		NONE = -1, VERTEX = 0, FRAGMENT = 1
+	};
+	std::ifstream stream(filePath);
+	std::string line;
+	std::stringstream ss[2];
+	ShaderType type = ShaderType::NONE;
+
+	
+	std::cout << "文件打开成功" << std::endl;
+	if (stream) {
+
+		while (getline(stream, line)) {
+
+			if (line.find("#shader") != std::string::npos)
+			{
+				if (line.find("vertex") != std::string::npos)
+				{
+					type = ShaderType::VERTEX;
+				}
+				else if (line.find("fragment") != std::string::npos)
+				{
+					type = ShaderType::FRAGMENT;
+				}
+
+			}
+			else
+			{
+				ss[(int)type] << line << "\n";
+			}
+		}
+	}
+	else {
+		std::cout << "Error :CheakPath" << filePath << std::endl;
+
+	}
+	return { ss[0].str(), ss[1].str() };
+}
+static unsigned int CompileShader(unsigned int type, const std::string& source)
+{
+	unsigned int id = glCreateShader(type);
+	const char* src = source.c_str();
+	glShaderSource(id, 1, &src, nullptr);
+	glCompileShader(id);
+
+	int result;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+	if (result == GL_FALSE) {
+		int length;
+		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+		char* message = (char*)alloca(length * sizeof(char));
+		glGetShaderInfoLog(id, length, &length, message);
+
+		std::cout << "failed to compile" << (type == GL_VERTEX_SHADER ? "vertex" : "fragemnt") << std::endl;
+		std::cout << message << std::endl;
+
+		glDeleteShader(id);
+		return 0;
+	}
+
+	return id;
+
+}
+static int CreatShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+	unsigned int program = glCreateProgram();
+	unsigned int vs =  CompileShader(GL_VERTEX_SHADER, vertexShader);
+	unsigned int fs = CompileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+
+	glLinkProgram(program );
+	glValidateProgram(program);
+
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+	return program;
+
+}
+
+
+
+
+int main()
 {
     GLFWwindow* window;
 
-	//初始化GLFW库
     if (!glfwInit())
         return -1;
 
-    //创建一个窗口，被window指针接受
     window = glfwCreateWindow(1920, 1080, "Hello World", NULL, NULL);
-    if (!window)//如果创建失败
+    if (!window)
     {
         glfwTerminate();
         return -1;
     } 
 
-	//将窗口的上下文设置为当前线程的主上下文
     glfwMakeContextCurrent(window);
-
-
-	//初始化GLEW库
 
     if(glewInit() != GLEW_OK)
     {
@@ -40,28 +131,35 @@ int main(void)
 	};//在内存空间定义了一组数据，后续会发送到GPU中进行处理
 
     unsigned int a;
-	glGenBuffers(1, &a);//生成一个缓冲对象，并将其ID存储在变量a中
+	glGenBuffers(1, &a);
 	glBindBuffer(GL_ARRAY_BUFFER, a);//指定a缓冲区的类型为GL_ARRAY_BUFFER，并将其绑定到当前上下文中
 	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), position, GL_STATIC_DRAW);//将数据复制到当前绑定的缓冲区对象中，数据大小为6个float，数据来源于position数组，使用GL_STATIC_DRAW作为使用模式
 
+	glEnableVertexAttribArray(0);//启用顶点属性数组，参数0表示启用索引值为0的顶点属性数组
 
-   //循环直到用户关闭窗口
+	//给第0个顶点属性定义读取方式
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
 
+	ShaderProgramResource source = ParseShader("res/shaders/BasicalShader.shader");
+
+	unsigned int shader = CreatShader(source.VertexShader ,source.FragmentShader);
+	std::cout << source.FragmentShader;
+	glUseProgram(shader);
     while (!glfwWindowShouldClose(window))
     {
-		//在这里我们可以进行渲染操作
+		//在这里可以进行渲染操作
         glClear(GL_COLOR_BUFFER_BIT);
 
-        
+         
 		glDrawArrays(GL_TRIANGLES, 0, 3);//绘制调用。GL_TRIANGLES表示绘制模式，0表示从数组的第一个顶点开始，3表示绘制3个顶点
 
 		//交换前后缓冲区，以显示渲染结果
         glfwSwapBuffers(window);
 
-		//检查是否有事件被触发（比如键盘输入、鼠标移动等），并调用相应的回调函数
-        glfwPollEvents();
+		  glfwPollEvents();
     }
 
+	glDeleteProgram(shader);
 	glfwTerminate();//终止GLFW库，清理资源
     return 0;
 }
